100339a60d3e29512e618f9d54b27507a1c415d6
jweeeee <92130806+jweeeee@users.noreply.github.com>
2025-06-24 15:42:59 +0900

    Update main.py

--- fastapi_webapp/main.py
+++ fastapi_webapp/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -7,10 +6,6 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
+@app.get("/")
+def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-@app.get("/order", response_class=HTMLResponse)
-async def order_page(request: Request):
-    return templates.TemplateResponse("order.html", {"request": request})
